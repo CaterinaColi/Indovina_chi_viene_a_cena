ed572f7b4b566728a09b18317b224ab3115a6db1
Caterina Colì <31853400+CaterinaColi@users.noreply.github.com>
2021-06-06 18:17:49 +0200

    Script di R
    Script da utilizzare sul vostro computer se volete usarlo sul vostro computer

---
+++ Indovina chi viene a cena_R tutorial_1.R
@@ -0,0 +1,45 @@
+##possibili invitati####
+
+Gianna = list("sesso"= "F","anni"= 60, "nazionalità" ="italiana", "vacanze" = "mare","vuole_viaggiare"=T)
+Mauro = list("sesso"= "M", "anni"= 29,"nazionalità" ="brasiliana", "vacanze" = "montagna","vuole_viaggiare"=F)
+Beatrice = list("sesso"= "F","anni"= 28, "nazionalità" ="spagnola", "vacanze" = "lago","vuole_viaggiare"=T)
+Lina = list("sesso"= "F","anni"= 28, "nazionalità" ="francese", "vacanze" = "città","vuole_viaggiare"=T)
+Roberto = list("sesso"= "F","anni"= 28, "nazionalità" ="portoghese", "vacanze" = "mare","vuole_viaggiare"=T)
+
+##creaiamo una lista di candidati####
+candidati = list("Gianna" = Gianna,"Mauro"=Mauro,"Beatrice"=Beatrice, "Lina"=Lina,"Roberto"=Roberto)
+
+##scegliete il vostro candidato ideale####
+candidato_ideale = list(
+"sesso" = c("F","M"),
+"anni"= c(27,28,29,30),
+"nazionalità"= c("italiana","spagnola","portoghese"),
+"vacanze" = c("mare","lago"))
+
+##codice che ci aiuta a scegliere i nostri invitati####
+
+invitati <- c()
+for(candidato in 1:length(candidati)){
+  if(candidati[[candidato]][['sesso']] %in% candidato_ideale$sesso){
+    if(candidati[[candidato]][['anni']] %in% candidato_ideale$anni){
+      if(candidati[[candidato]][['nazionalità']] %in% candidato_ideale$nazionalità){
+        if(candidati[[candidato]][['vacanze']] %in% candidato_ideale$vacanze){
+          
+          
+          invitati <- c(invitati,(names(candidati[candidato])))
+          
+        }
+      }
+    }
+    
+    
+  }
+  
+}
+
+
+##Grafico a torta delle persone che verranno invitate a cena####
+
+pie(rep(1,length(invitati)), invitati,col = rainbow(5))
+
+
