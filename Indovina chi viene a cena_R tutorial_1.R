c66ccf791ad314766c43745505f102b3ecd2d2f2
Caterina Colì <31853400+CaterinaColi@users.noreply.github.com>
2021-06-13 17:15:17 +0200

    Update Indovina chi viene a cena_R tutorial_1.R
    add descriprion to pie chart

--- Indovina chi viene a cena_R tutorial_1.R
+++ Indovina chi viene a cena_R tutorial_1.R
@@ -1,10 +1,10 @@
 ##possibili invitati####
 
-Gianna = list("sesso"= "F","anni"= 60, "nazionalit‡" ="italiana", "vacanze" = "mare","vuole_viaggiare"=T)
-Mauro = list("sesso"= "M", "anni"= 29,"nazionalit‡" ="brasiliana", "vacanze" = "montagna","vuole_viaggiare"=F)
-Beatrice = list("sesso"= "F","anni"= 28, "nazionalit‡" ="spagnola", "vacanze" = "lago","vuole_viaggiare"=T)
-Lina = list("sesso"= "F","anni"= 28, "nazionalit‡" ="francese", "vacanze" = "citt‡","vuole_viaggiare"=T)
-Roberto = list("sesso"= "F","anni"= 28, "nazionalit‡" ="portoghese", "vacanze" = "mare","vuole_viaggiare"=T)
+Gianna = list("sesso"= "F","anni"= 60, "nazionalit√†" ="italiana", "vacanze" = "mare","vuole_viaggiare"=T)
+Mauro = list("sesso"= "M", "anni"= 29,"nazionalit√†" ="brasiliana", "vacanze" = "montagna","vuole_viaggiare"=F)
+Beatrice = list("sesso"= "F","anni"= 28, "nazionalit√†" ="spagnola", "vacanze" = "lago","vuole_viaggiare"=T)
+Lina = list("sesso"= "F","anni"= 28, "nazionalit√†" ="francese", "vacanze" = "citt√†","vuole_viaggiare"=T)
+Roberto = list("sesso"= "F","anni"= 28, "nazionalit√†" ="portoghese", "vacanze" = "mare","vuole_viaggiare"=T)
 
 ##creaiamo una lista di candidati####
 candidati = list("Gianna" = Gianna,"Mauro"=Mauro,"Beatrice"=Beatrice, "Lina"=Lina,"Roberto"=Roberto)
@@ -13,7 +13,7 @@ candidati = list("Gianna" = Gianna,"Mauro"=Mauro,"Beatrice"=Beatrice, "Lina"=Lin
 candidato_ideale = list(
 "sesso" = c("F","M"),
 "anni"= c(27,28,29,30),
-"nazionalit‡"= c("italiana","spagnola","portoghese"),
+"nazionalit√†"= c("italiana","spagnola","portoghese"),
 "vacanze" = c("mare","lago"))
 
 ##codice che ci aiuta a scegliere i nostri invitati####
@@ -22,7 +22,7 @@ invitati <- c()
 for(candidato in 1:length(candidati)){
   if(candidati[[candidato]][['sesso']] %in% candidato_ideale$sesso){
     if(candidati[[candidato]][['anni']] %in% candidato_ideale$anni){
-      if(candidati[[candidato]][['nazionalit‡']] %in% candidato_ideale$nazionalit‡){
+      if(candidati[[candidato]][['nazionalit√†']] %in% candidato_ideale$nazionalit√†){
         if(candidati[[candidato]][['vacanze']] %in% candidato_ideale$vacanze){
           
           
@@ -40,6 +40,6 @@ for(candidato in 1:length(candidati)){
 
 ##Grafico a torta delle persone che verranno invitate a cena####
 
-pie(rep(1,length(invitati)), invitati,col = rainbow(5))
+pie(x= rep(1,length(invitati)), labels= invitati,col = rainbow(5))
 
 
